744723c601b5762e326995be33313bfc8d11c6b5
AngomRanjan <angomranjan@gmail.com>
2022-04-01 04:11:39 +0530

    added onclick function for reset button

--- etch.js
+++ etch.js
@@ -13,9 +13,28 @@ function createUnitPixel(uSize)
         container.appendChild(content[i]);
     }
 }
+function delUnit(){
+    const container = document.querySelector('#canvas');
+    container.textContent="";
+}
+function addEvLis(){
+    
+    document.querySelectorAll('.unit').forEach(item => {
+        item.addEventListener('mouseover', event =>
+          item.style.backgroundColor='red')
+      });
 
-createUnitPixel(16);
-var a=document.querySelectorAll('.unit').forEach(item => {
-    item.addEventListener('mouseover', event =>
-      item.style.backgroundColor='red')
-  });
+}
+function reset() {
+    delUnit();
+    sqrs=-1;
+    while(sqrs<0 ||sqrs>100||isNaN(sqrs)|| !(/^\d+$/.test(sqrs))) 
+        sqrs=prompt("Number Of Squares per Side : ","Enter a number between 1 to 100");
+    createUnitPixel(sqrs);
+    addEvLis();
+}
+
+var sqrs=5;
+createUnitPixel(sqrs);
+addEvLis();
+document.getElementById("reset").onclick = () =>reset();
